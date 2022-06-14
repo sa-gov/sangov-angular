5e7e95bada74512f050de100d63c781226c3b011
Harrison Mayotte <hmayotte@mdhholdings.org>
2022-06-14 13:05:29 -0400

    Reference updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IndexComponent} from "./pages/index/index.component";
 import {AboutComponent} from "./pages/about/about.component";
+import { GettingStartedComponent } from './pages/getting-started/getting-started.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { CdnComponent } from './pages/cdn/cdn.component';
+import { StructureComponent } from './pages/structure/structure.component';
 
 const routes: Routes = [
   {path: 'home', component: IndexComponent},
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'getting-started', component: GettingStartedComponent},
+  {path: 'update', component: UpdateComponent},
+  {path: 'cdn', component: CdnComponent},
+  {path: 'structure', component: StructureComponent},
+  // {path: 'sample', component: },
+  // {path: '', component: },
+  // {path: '', component: },
+  // {path: '', component: },
+  // {path: '', component: },
+  // {path: '', component: },
 ];
 
 @NgModule({
